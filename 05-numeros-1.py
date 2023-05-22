10157a341d361eba6d84fa05f67666cee728c06d
JuanF31 <sebashernandez03@hotmail.com>
2023-05-21 23:40:19 -0600

    Operadores

---
+++ 05-numeros-1.py
@@ -0,0 +1,21 @@
+def suma(a = 0, b = 0):
+    print(a + b)
+
+suma(10, 2)
+suma(4, 1)
+suma(8)
+
+def resta(a = 0, b = 0):
+    print(a - b)
+
+resta(10, 8)
+
+def multiplicacion(a = 0, b = 0):
+    print(a * b)
+
+multiplicacion(5, 4)
+
+def division(a = 0, b = 0):
+    print(a / b)
+
+division(10, 5)
---
+++ 05-numeros.py
@@ -0,0 +1,19 @@
+print(2)
+print(4)
+
+print(2.5)
+
+print(2 + 3)
+print(8 - 4)
+print(1 * 4)
+print(10 / 3)
+# 4 * 4 * 4 = 64
+print(4 ** 3)
+# 3 * 3 * 3 = 27
+print(3 ** 3)
+
+print((2 + 3) * 4)
+
+numero = 20
+numero += 1
+print(numero)
