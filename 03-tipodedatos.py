0ae79ad25be6fcbf8828a30957bfe201d90e9a39
JuanF31 <sebashernandez03@hotmail.com>
2023-05-21 23:44:39 -0600

    Comentarios

--- 03-tipodedatos.py
+++ 03-tipodedatos.py
@@ -1,12 +1,16 @@
+# Strings utilizan comillas dobles o comillas simples
 nombre = "Juan Francisco"
 print(nombre)
 
+# Enteros
 edad = 18
 edad = 23
 print(edad)
 
+# Floats o Flotantes
 cantidad_a_pagar = 100.50
 print(cantidad_a_pagar)
 
+# Booleano es True o False
 pagado = True
 print(pagado)
--- 04-funciones-3.py
+++ 04-funciones-3.py
@@ -1,5 +1,6 @@
 def informacion(nombre):
     return nombre
 
+# empleado tendra la informacion pasada a la funcion informacion()
 empleado = informacion("Juan")
 print(empleado)
